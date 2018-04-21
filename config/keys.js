bf4e5f531b067382b4d7ca3df27938ee410dafa0
Vasyl <vasylpb89@gmail.com>
2018-04-21 18:18:57 +0300

    Add JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://Vasyl:Qw12Op34@ds247479.mlab.com:47479/mlabdb89'
+	mongoURI: 'mongodb://Vasyl:Qw12Op34@ds247479.mlab.com:47479/mlabdb89',
+	secretOrKey: 'secret'
 };
